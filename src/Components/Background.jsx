197fa4e071aefaa0a3ed6af760aff5204dd5d644
Werosh Kriyanjala <weroshprofy@gmail.com>
2024-12-20 15:16:17 +0530

    images Path Changed

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -4,7 +4,7 @@ const Background = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full z-[-1]">
       <img
-        src="./src/assets/back-2.jpg"
+        src="src/assets/back-2.jpg"
         alt="Background"
         className="object-cover w-full h-full"
       />
