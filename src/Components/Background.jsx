a53f8092bd2a95e568fdc5f5f63c6d66365f1316
Werosh Kriyanjala <weroshprofy@gmail.com>
2024-12-20 16:27:15 +0530

    test

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import imgBackground from "../assets/back-2.jpg"
 
 const Background = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full z-[-1]">
       <img
-        src="src/assets/back-2.jpg"
+        src={imgBackground}
         alt="Background"
         className="object-cover w-full h-full"
       />
